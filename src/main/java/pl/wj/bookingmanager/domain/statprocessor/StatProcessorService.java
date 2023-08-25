d6aeb8a6d15695f2bd2a864a33ad3ea9d655a645
jankowskiw <waldemar.jankowski95@gmail.com>
2023-08-25 17:07:53 +0200

    Optimize imports

--- src/main/java/pl/wj/bookingmanager/domain/statprocessor/StatProcessorService.java
+++ src/main/java/pl/wj/bookingmanager/domain/statprocessor/StatProcessorService.java
@@ -2,7 +2,6 @@ package pl.wj.bookingmanager.domain.statprocessor;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
 @Service
 @RequiredArgsConstructor
