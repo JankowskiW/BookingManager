4e089ce48228ec938ec212016e882b4b1de88234
jankowskiw <waldemar.jankowski95@gmail.com>
2023-08-26 13:50:56 +0200

    Add pagination for getUsers() method.

--- src/main/java/pl/wj/bookingmanager/domain/userprocessor/UserProcessorController.java
+++ src/main/java/pl/wj/bookingmanager/domain/userprocessor/UserProcessorController.java
@@ -3,6 +3,10 @@ package pl.wj.bookingmanager.domain.userprocessor;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.wj.bookingmanager.domain.userprocessor.model.User;
@@ -13,8 +17,6 @@ import pl.wj.bookingmanager.domain.userprocessor.model.dto.UserUpdateRequestDto;
 import pl.wj.bookingmanager.infrastructure.security.SecurityService;
 import pl.wj.bookingmanager.infrastructure.security.model.dto.JwtResponseDto;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -23,8 +25,11 @@ public class UserProcessorController {
     private final SecurityService securityService;
 
     @GetMapping
-    public List<User> getUsers() {
-        return userProcessorService.getUsers();
+    public Page<User> getUsers(@RequestParam int pageNumber, @RequestParam int pageSize,
+                               @RequestParam Sort.Direction direction, @RequestParam String directionField) {
+        // page number should be decremented, because PageRequest starts counting pages from 0, not from 1
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, direction, directionField);
+        return userProcessorService.getUsers(pageable);
     }
 
     @PostMapping("/register")
@@ -45,7 +50,7 @@ public class UserProcessorController {
                                You have to pass <b>complete user model</b>. Fields that you do not want to update, leave unedited but filled.<br>
                                If you will pass empty field, in worst scenario it will update that field to default value.<br><br>
                                Returns updated user without password field.""")
-    public UserResponseDto updateUser(@RequestParam long id, @Valid @RequestBody UserUpdateRequestDto userUpdateRequestDto) {
+    public UserResponseDto updateUser(@PathVariable long id, @Valid @RequestBody UserUpdateRequestDto userUpdateRequestDto) {
         return userProcessorService.updateUser(id, userUpdateRequestDto);
     }
 
--- src/main/java/pl/wj/bookingmanager/domain/userprocessor/UserProcessorService.java
+++ src/main/java/pl/wj/bookingmanager/domain/userprocessor/UserProcessorService.java
@@ -1,6 +1,8 @@
 package pl.wj.bookingmanager.domain.userprocessor;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.wj.bookingmanager.domain.userprocessor.model.User;
@@ -10,28 +12,27 @@ import pl.wj.bookingmanager.domain.userprocessor.model.dto.UserResponseDto;
 import pl.wj.bookingmanager.domain.userprocessor.model.dto.UserSecurityDto;
 import pl.wj.bookingmanager.domain.userprocessor.model.dto.UserUpdateRequestDto;
 import pl.wj.bookingmanager.infrastructure.exception.definition.ResourceAlreadyExistsException;
-
-import java.util.List;
+import pl.wj.bookingmanager.infrastructure.exception.definition.ResourceNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class UserProcessorService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+
     public UserSecurityDto getUserByUsername(String username) {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Resource not found exception"));    // TODO: Make custom exceptions handler
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new ResourceNotFoundException("User with username " + username + " not found"));
         return UserMapper.toUserSecurityDto(UserMapper.toCustomUserDetails(user));
     }
 
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public Page<User> getUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
     public UserResponseDto addUser(UserRegisterRequestDto userRegisterRequestDto) {
-        if (userRepository.existsByUsername(userRegisterRequestDto.username()))
-            throw new ResourceAlreadyExistsException("User with username " + userRegisterRequestDto.username() + " already exists");
-        if (userRepository.existsByEmailAddress(userRegisterRequestDto.emailAddress()))
-            throw new ResourceAlreadyExistsException("User with email address " + userRegisterRequestDto.emailAddress() + " already exists");
+        throwIfNewUserExists(userRegisterRequestDto);
         String encodedPassword = passwordEncoder.encode(userRegisterRequestDto.password());
         User user = UserMapper.toUser(userRegisterRequestDto, encodedPassword);
         user = userRepository.save(user);
@@ -39,13 +40,24 @@ public class UserProcessorService {
     }
 
     public UserResponseDto updateUser(long id, UserUpdateRequestDto userUpdateRequestDto) {
-        if (userRepository.existsByUsernameAndIdIsNot(userUpdateRequestDto.username(), id))
-            throw new ResourceAlreadyExistsException("User with username " + userUpdateRequestDto.username() + " already exists");
-        if (userRepository.existsByEmailAddressAndIdIsNot(userUpdateRequestDto.emailAddress(), id))
-            throw new ResourceAlreadyExistsException("User with email address " + userUpdateRequestDto.emailAddress() + " already exists");
+        throwIfUpdatedUserExists(id, userUpdateRequestDto);
         String encodedPassword = passwordEncoder.encode(userUpdateRequestDto.password());
         User user = UserMapper.toUser(id, userUpdateRequestDto, encodedPassword);
         user = userRepository.save(user);
         return UserMapper.toUserResponseDto(user);
+    }
+
+    private void throwIfNewUserExists(UserRegisterRequestDto userRegisterRequestDto) {
+        if (userRepository.existsByUsername(userRegisterRequestDto.username()))
+            throw new ResourceAlreadyExistsException("User with username " + userRegisterRequestDto.username() + " already exists");
+        if (userRepository.existsByEmailAddress(userRegisterRequestDto.emailAddress()))
+            throw new ResourceAlreadyExistsException("User with email address " + userRegisterRequestDto.emailAddress() + " already exists");
+    }
+
+    private void throwIfUpdatedUserExists(long id, UserUpdateRequestDto userUpdateRequestDto) {
+        if (userRepository.existsByUsernameAndIdIsNot(userUpdateRequestDto.username(), id))
+            throw new ResourceAlreadyExistsException("User with username " + userUpdateRequestDto.username() + " already exists");
+        if (userRepository.existsByEmailAddressAndIdIsNot(userUpdateRequestDto.emailAddress(), id))
+            throw new ResourceAlreadyExistsException("User with email address " + userUpdateRequestDto.emailAddress() + " already exists");
     }
 }
