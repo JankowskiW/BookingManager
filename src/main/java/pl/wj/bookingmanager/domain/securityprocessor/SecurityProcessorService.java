dd23c4d3cc2e9f9cb34a5e24b4f9461d33d198ad
jankowskiw <waldemar.jankowski95@gmail.com>
2023-08-26 14:05:09 +0200

    Delete unnecessary security module from domain package

--- src/main/java/pl/wj/bookingmanager/domain/securityprocessor/SecurityProcessorService.java
+++
@@ -1,9 +0,0 @@
-package pl.wj.bookingmanager.domain.securityprocessor;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class SecurityProcessorService {
-}
