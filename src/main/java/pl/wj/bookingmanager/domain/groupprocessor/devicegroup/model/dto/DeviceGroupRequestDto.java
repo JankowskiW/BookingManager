2c2172856058f1abf697a1fd19b5cad4c4cb7f3c
jankowskiw <waldemar.jankowski95@gmail.com>
2023-09-05 21:07:55 +0200

    Add validation for groups.

--- src/main/java/pl/wj/bookingmanager/domain/groupprocessor/devicegroup/model/dto/DeviceGroupRequestDto.java
+++ src/main/java/pl/wj/bookingmanager/domain/groupprocessor/devicegroup/model/dto/DeviceGroupRequestDto.java
@@ -1,10 +1,15 @@
 package pl.wj.bookingmanager.domain.groupprocessor.devicegroup.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record DeviceGroupRequestDto(
+        @NotBlank(message = "{device-group.name.not-blank}")
         String name,
+        @NotBlank(message = "{device-group.description.not-blank}")
         String description,
+        @NotNull(message = "{device-group.available.not-null}")
         boolean available
 ) {}
--- src/main/java/pl/wj/bookingmanager/domain/groupprocessor/roomgroup/model/dto/RoomGroupRequestDto.java
+++ src/main/java/pl/wj/bookingmanager/domain/groupprocessor/roomgroup/model/dto/RoomGroupRequestDto.java
@@ -1,10 +1,15 @@
 package pl.wj.bookingmanager.domain.groupprocessor.roomgroup.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record RoomGroupRequestDto(
+        @NotBlank(message = "{room-group.name.not-blank}")
         String name,
+        @NotBlank(message = "{room-group.description.not-blank}")
         String description,
+        @NotNull(message = "{room-group.available.not-null}")
         boolean available
 ) {}
